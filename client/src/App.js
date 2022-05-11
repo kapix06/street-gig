9070c9655823eb73c600cc07d7f6584e56467d0c
Kalina Jedrzychowska <kalina.jedrzychowska@gmail.com>
2022-05-11 12:44:07 +0200

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './App.css';
+import HomePage from './pages/HomePage';
+import Navbar from './components/Navbar';
+import { Routes, Route } from "react-router-dom";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      <Navbar />
+      <Routes>  
+      <Route path="/" element={ <HomePage /> } />
+      </Routes>  
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
