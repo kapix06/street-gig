231b5e9c6bbc68cf0505798654015295c14d84e5
Kalina Jedrzychowska <kalina.jedrzychowska@gmail.com>
2022-05-19 17:35:02 +0200

    finaltest

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ require("dotenv/config");
 require("./db");
 
 
+
+
 const { isAuthenticated } = require('./middleware/jwt')
 
 // Handles http requests (express is node js framework)
@@ -26,7 +28,19 @@ app.use("/api" ,allRoutes);
 const auth = require("./routes/auth");
 app.use("/api/auth", auth);
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+
+app.use((req, res) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(__dirname + "/client/build/index.html");
+  });
+
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
 module.exports = app;
+
+
